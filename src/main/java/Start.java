6bf81dd63ca74937cef76fd18eaf0d72087daa33
Yanislav Kornev <zverijo@gmail.com>
2016-04-15 15:45:40 +0300

    Проверочные коммиты для проверки слияния.

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -5,10 +5,12 @@ public class Start {
     public static void main(String[] args) {
 
         Settings.getSettings();
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            JFrame.setDefaultLookAndFeelDecorated(true);
-            JDialog.setDefaultLookAndFeelDecorated(true);
-            new jFrame();
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame.setDefaultLookAndFeelDecorated(true);
+                JDialog.setDefaultLookAndFeelDecorated(true);
+                new jFrame();
+            }
         });
     }
 }
