7c05e2bc5ca2189bd0ebb7f8fb792b770197dc83
Вадим <vadim0872@mail.ru>
2016-04-27 21:51:18 +0400

    Изменение структуры

--- src/main/java/Sms/SendSms.java
+++ src/main/java/Sms/SendSms.java
@@ -1,12 +1,38 @@
 package Sms;
 
+import Exeptions.*;
+import parsing.students.Student;
+
+import java.util.ArrayList;
+
 /**
  * Created by Вадим on 20.04.2016.
  */
-public class SendSms implements Runnable{
+public class SendSms extends Thread{
+
+    private String service;
+    private ArrayList<Student> list;
+    private String login;
+    private String password;
+    private String message;
+    private String from;
+
+    SendSms(String service){
+        this.service = service;
+        this.list = list;
+        this.login = login;
+        this.password = password;
+        this.message = message;
+        this.from = from;
+    }
 
     @Override
     public void run() {
-
+       switch (service){
+           case "SmsRu": {
+               SmsRu smsRu = new SmsRu(list,login,password,message);
+            //   smsRu.CreateGroup();
+           }
+       }
     }
 }
