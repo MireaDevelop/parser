054898002071426b739b4d2fe277b2081ca87a27
Вадим <vadim0872@mail.ru>
2016-05-06 01:15:44 +0400

    fix some bugs

--- src/main/java/org/Sms/SmsRu.java
+++ src/main/java/org/Sms/SmsRu.java
@@ -12,16 +12,24 @@ import java.util.ArrayList;
 
 /**
  * Created by Вадим on 20.04.2016.
+ * Для отправки сообщений вызвать метод SendMsg и перехватить соответстующие исключения;
+ * Сообщения отправляются пачками, до 100шт за раз;
+ * Если поле from не было заполненно, т.е. = "", то автоматически вставится номер(он же логин) отправителя;
  */
-public class SmsRu extends SmsServices {
+public final class SmsRu extends SmsServices {
 
 
-    SmsRu(ArrayList<Student> list, String login, String password, String message){
-        super(list,login,password,message);
+    SmsRu(ArrayList<Student> list, String login, String password, String message, String from){
+        super(list,login,password,message,from);
+        if (from.equals("")){
+            this.from = login;
+        }
     }
 
+   
 
-    public void SendMsg() throws ServiceNotWork, AuthFailed, NotConfirmLogin, TooBigSms, NonText, NotEnoughMoney {
+    public void SendMsg() throws ServiceNotWork, AuthFailed, NotConfirmLogin, TooBigSms, NonText,
+            NotEnoughMoney, NotConfirmSenderName {
 
         String numbers="";
         int count=0;
@@ -60,53 +68,61 @@ public class SmsRu extends SmsServices {
 
 
     @Override
-    public boolean Send(String to) throws ServiceNotWork, NonText, AuthFailed, NotConfirmLogin, TooBigSms {
+    public boolean Send(String to) throws ServiceNotWork, NonText, AuthFailed, NotConfirmLogin, TooBigSms,
+            NotConfirmSenderName, NotEnoughMoney {
 
         ArrayList<String> response;
 
-        final String request = "http://sms.ru/sms/send?login=" + login + "&password=" + password + "&to=" + to + "&text=" + message;//"&test=1"
+
+
+        final String request = "http://sms.ru/sms/send?login=" + login
+                + "&password=" + password
+                + "&to=" + to
+                + "&text=" + message;//+"&test=1";
         response = sendrequest(request);
 
-        if (response.get(0) == "100"){
+        if (response.get(0).equals("100")){
             return true;
-        } if (response.get(0) != "100"){
+        } else{
             throwError(response.get(0));
             return false;
         }
-
-        return false;
     }
 
-    protected boolean checkbill() throws ServiceNotWork, NotConfirmLogin, AuthFailed, TooBigSms, NonText {
+    protected boolean checkbill() throws ServiceNotWork, NotConfirmLogin, AuthFailed, TooBigSms, NonText,
+            NotConfirmSenderName, NotEnoughMoney {
+
         ArrayList<String> responecost,responsebalance;
+
         double balance = 0;
         double cost = 0;
-        final String requestbalance = "http://sms.ru/my/balance?login=" + login + "&password=" + password;
-        final String requsestcost = "http://sms.ru/sms/cost?login=" + login + "&password=" + password + "&to=" + list.get(0).getPhone() + "&text=" + message;
-        //final String testcost = "http://sms.ru/sms/cost?login=" + login + "&password=" + password + "&to=" + "79156666666" + "&text=" + message;
+
+        final String requestbalance = "http://sms.ru/my/balance?login=" + login
+                + "&password=" + password;
+        final String requsestcost = "http://sms.ru/sms/cost?login=" + login
+                + "&password=" + password
+                + "&to=" + list.get(0).getPhone()
+                + "&text=" + message;
 
         responecost = sendrequest(requsestcost);
         responsebalance =sendrequest(requestbalance);
 
-        if (responecost.get(0) == "100") {
+        if (responecost.get(0).equals("100")) {
             cost = Double.parseDouble(responecost.get(1));
-        }   else if (responecost.get(0) != "100"){
+        }   else {
             throwError(responecost.get(0));
         }
 
-        if (responsebalance.get(0) == "100"){
+        if (responsebalance.get(0).equals("100")){
             balance = Double.parseDouble(responsebalance.get(1));
-        } else if (responsebalance.get(0) != "100"){
+        } else {
             throwError(responsebalance.get(0));
         }
 
-        responecost = null;
-        responsebalance = null;
-
         if (balance > cost *list.size()){
             return true;
-        } else return false;
-
+        } else
+            return false;
     }
 
 
@@ -134,10 +150,17 @@ public class SmsRu extends SmsServices {
         return list;
     }
 
-    protected void throwError(String error) throws ServiceNotWork, AuthFailed, NotConfirmLogin, NonText, TooBigSms {
+    protected void throwError(String error) throws ServiceNotWork, AuthFailed, NotConfirmLogin, NonText, TooBigSms,
+            NotConfirmSenderName, NotEnoughMoney {
         switch (error){
+            case "201":
+                throw new NotEnoughMoney("Недостаточно средств на счете");
+
             case "203":
                 throw new NonText("Нет текста");
+
+            case "204":
+                throw  new NotConfirmSenderName("Подпись не подтверждена");
 
             case "205":
                 throw new TooBigSms("Слишком большое сообщение(превышает 8 смс)");
