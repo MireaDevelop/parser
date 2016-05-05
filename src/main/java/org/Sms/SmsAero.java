b8d6b4e9f7e103af853b4ecaaa735c98a413ec26
vadim0872 <vadim0872@mail.ru>
2016-05-06 01:19:38 +0400

    Итоговая интеграция  сервиса SmsAero

--- src/main/java/org/Sms/SmsAero.java
+++ src/main/java/org/Sms/SmsAero.java
@@ -1,40 +1,78 @@
 package org.Sms;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.exeptions.*;
 import org.students.Student;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
-import java.util.ArrayList;
+
 
 /**
  * Created by Вадим on 24.04.2016.
+ * Для отправки сообщений вызвать метод SendMsg и перехватить соответстующие исключения;
+ * Сообщения отправляются по одному;
+ *
  */
-public class SmsAero extends SmsServices {
+public final class SmsAero extends SmsServices {
 
     private static ArrayList<Student> list;
 
     public static void main(String[] args) {
-        SmsAero smsAero = new SmsAero(list,"qwery","qwery","Вот такое письмо я хочу увидеть!@#$%^&*()_+");
+
+        SmsAero smsAero = new SmsAero(list,"vadim0872@mail.ru","2lxy0c1","Проверка стоимости сообщения.","MireaDev");//8
         String s;
+        //smsAero.from = "MireaDev";
         s = smsAero.message;
         System.out.println(s);
         s = URLDecoder.decode(smsAero.message);
         System.out.println(s);
+        try {
+            //smsAero.Send("79175364142");
+            try {
+                smsAero.checkbill();
+            } catch (NotConfirmSenderName notConfirmSenderName) {
+                notConfirmSenderName.printStackTrace();
+            } catch (NotEnoughMoney notEnoughMoney) {
+                notEnoughMoney.printStackTrace();
+            }
+        } catch (ServiceNotWork serviceNotWork) {
+            serviceNotWork.printStackTrace();
+        } catch (NonText nonText) {
+            nonText.printStackTrace();
+        } catch (AuthFailed authFailed) {
+            authFailed.printStackTrace();
+        } catch (NotConfirmLogin notConfirmLogin) {
+            notConfirmLogin.printStackTrace();
+        } catch (TooBigSms tooBigSms) {
+            tooBigSms.printStackTrace();
+        }
+       /* Response response = smsAero.sendrequest("http://gate.smsaero.ru/send/?user=vadim0872@mail.ru&password=50d9fb15e0b5e11953be7f03f379880&to=79175364142&textПроверка стоимости сообщения. Тест, тест, тест, надо набрать больше 70 символов кириллицы, а лучше с запасом!=&from=NEWS&type=3&answer=json");
+        System.out.println(response.id);
+        System.out.println(response.result);
+        Response response1 = smsAero.sendrequest("https://gate.smsaero.ru/checktarif/?user=vadim0872@mail.ru&password=50d9fb15e0b5e11953be7f03f379880c&answer=json");
+        System.out.println(response1.reason.Direct_channel);
+        System.out.println(response1.result);*/
+
     }
 
 
-    public SmsAero(ArrayList<Student> list, String login, String password, String message) {
-        super(list, login, DigestUtils.md5Hex(password), message);
-        //this.password = DigestUtils.md5Hex(password);
+    public SmsAero(ArrayList<Student> list, String login, String password, String message,String from) {
+        super(list, login, DigestUtils.md5Hex(password), message,from);
     }
 
 
-    public void SendMsg() throws ServiceNotWork, AuthFailed, NotConfirmLogin, TooBigSms, NonText, NotEnoughMoney {
+    public void SendMsg() throws ServiceNotWork, AuthFailed, NotConfirmLogin, TooBigSms, NonText,
+            NotEnoughMoney, NotConfirmSenderName {
 
         if (checkbill()) {
             for (Student student : list) {
@@ -49,31 +87,76 @@ public class SmsAero extends SmsServices {
 
 
     @Override
-    protected boolean Send(String to )throws ServiceNotWork, NonText, AuthFailed, NotConfirmLogin, TooBigSms{
-        final String request = "https://gate.smsaero.ru/send/?user="+ login +"&password="+ password+ "&to="+ to +"&text="+ message+ "&answer=json";
-        sendrequest(request);
-        return true;
+    protected boolean Send(String to ) throws ServiceNotWork, NonText, AuthFailed, NotConfirmLogin, TooBigSms,
+            NotConfirmSenderName, NotEnoughMoney {
+        final String request = "https://gate.smsaero.ru/send/?user="+ login
+                +"&password=" + password
+                + "&to="+ to
+                +"&text="+ message
+                + "&from="+ from
+                +"&type=2&answer=json";
+
+        Response response = sendrequest(request);
+
+        if (response.getResult().equals("accepted")){
+            return true;
+        } else {
+            throwError(response.getReason());
+            return false;
+        }
     }
 
     @Override
-    protected boolean checkbill() throws ServiceNotWork, NotConfirmLogin, AuthFailed, TooBigSms, NonText {
+    protected boolean checkbill() throws ServiceNotWork, NotConfirmLogin, AuthFailed, TooBigSms, NonText,
+            NotConfirmSenderName, NotEnoughMoney {
+
         double balance = 0;
         double cost = 0;
-        final String requestbalance = "https://gate.smsaero.ru/balance/?user="+login+"&password="+password+"&answer=json";
-        final String requestcost="https://gate.smsaero.ru/checktarif/?user="+login+"&password="+password+"&answer=json";
+        int size = URLDecoder.decode(message).length() / 70 + 1;
 
-        return false;
+        Response responsecost,responsebalance;
+
+        final String requestbalance = "https://gate.smsaero.ru/balance/?user=" +login
+                + "&password="+password
+                + "&answer=json";
+        final String requestcost="https://gate.smsaero.ru/checktarif/?user=" + login
+                + "&password=" + password
+                + "&answer=json";
+
+
+        responsecost = sendrequest(requestcost);
+
+        if (responsecost.getResult().equals("accepted")) {
+            cost = Double.parseDouble(responsecost.getDirect_channel());
+        } else{
+            throwError(responsecost.getReason());
+        }
+
+        responsebalance = sendrequest(requestbalance);
+
+        if (responsebalance.getResult().equals("accepted")){
+            balance = Double.parseDouble(responsebalance.getBalance());
+        } else {
+            throwError(responsebalance.getReason());
+        }
+
+        if (balance > cost * size * list.size()){
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
 
     @Override
-    protected ArrayList<String> sendrequest(String urlToRead) {
+    protected Response sendrequest(String urlToRead) {
         URL url;
         HttpURLConnection connection;
         BufferedReader rd;
         String line;
-        ArrayList<String> list = new ArrayList<>();
+        String result="";
+
         try {
             url = new URL(urlToRead);
             connection = (HttpURLConnection) url.openConnection();
@@ -81,18 +164,97 @@ public class SmsAero extends SmsServices {
 
             rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             while ((line = rd.readLine()) != null) {
-                list.add(line);
+                result +=line;
             }
             rd.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return list;
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(Response.class, new SmsAeroDeserializer());
+        Gson gson = builder.create();
+        Response response = gson.fromJson(result, Response.class);
+
+        return response;
     }
 
     @Override
-    protected void throwError(String error) throws ServiceNotWork, AuthFailed, NotConfirmLogin, NonText, TooBigSms {
+    protected void throwError(String error) throws ServiceNotWork, AuthFailed, NotConfirmLogin, NonText, TooBigSms, NotConfirmSenderName, NotEnoughMoney {
+        switch (error){
+            case "incorrect user or password":
+                throw new AuthFailed("Неправильный логин или пароль");
 
+            case "incorrect sender name":
+                throw  new NotConfirmSenderName("Подпись не подтверждена");
+
+            case "no credits":
+                throw new NotEnoughMoney("Недостаточно средств на счете");
+
+        }
     }
+
+
+
+
 }
+class Response{
+
+    private String result;
+    private String reason;
+    private String Direct_channel;
+    private String Digital_channel;
+    private String balance;
+    private String id;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getDigital_channel() {
+        return Digital_channel;
+    }
+
+    public void setDigital_channel(String digital_channel) {
+        Digital_channel = digital_channel;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDirect_channel() {
+        return Direct_channel;
+    }
+
+    public void setDirect_channel(String direct_channel) {
+        Direct_channel = direct_channel;
+    }
+
+}
+
+
