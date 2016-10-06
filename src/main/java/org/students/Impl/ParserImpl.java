7c4645b6c1eedf00f3cae773efebbad8772ed624
vadim0872 <vadim0872@mail.ru>
2016-10-07 01:17:08 +0400

    Оптимизация работы парсера

--- src/main/java/org/students/Impl/ParserImpl.java
+++ src/main/java/org/students/Impl/ParserImpl.java
@@ -2,64 +2,132 @@ package org.students.Impl;
 
 
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.students.Parser;
 import org.students.Student;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 
 public class ParserImpl implements Parser {
+
          static int emailindex;
          static int vkindex;
          static int phoneindex;
 
+    public static void main(String[] args) {
 
-    public static ArrayList<Student> getStudents (String file,int iname, int iSurname, int iemail, int iid,int iphone )throws IOException{
+        ArrayList<Student> list = new ArrayList<>();
+        ArrayList<String> titels = null;
+        titels = getTitles("test.xlsx");
+        int i = 0;
+        for (String s:titels){
+            System.out.println("Заголовок № "+i+" зоголовок "+s);
+            i++;
+        }
+
+        list = getStudents("test.xlsx",1,2,3,4);
+
+        for (Student student: list) {
+            System.out.println("имя " + student.getName() + " телефон " + student.getPhone()+ "vk id: " +student.getVk_id());
+            student.setIsSendVk();
+        }
+
+        log("test.xlsx", list, true, true,false );
+
+        /*ArrayList<Student> list = null;
+        ArrayList<Long> longlist = new ArrayList<>();
+        long time2 = System.currentTimeMillis();
+        for (int i = 0; i <100 ; i++) {
+            long time = System.currentTimeMillis();
+            getTitles("test.xlsx");
+            list = getStudents("test.xlsx",1,2,3,4);
+            *//*for (Student student: list){
+                System.out.println("имя " + student.getName()+" телефон "+student.getPhone());
+            }*//*
+
+            log("test.xlsx",list,true, true,false);
+            time = System.currentTimeMillis() - time;
+            //System.out.println("время выполнения логирования с буфером "+time);
+            longlist.add(new Long(time));
+
+        }*/
+        /*long min = 1000;
+        long max = 0;
+        for (int i = 0; i <longlist.size() ; i++) {
+            if (longlist.get(i).longValue() < min){
+                min = longlist.get(i).longValue();
+            }
+
+            if (longlist.get(i).longValue() > max){
+                max = longlist.get(i).longValue();
+            }
+        }
+        System.out.println("Минмальное время: " + min);
+        System.out.println("Максимальное время: " + max);
+
+        time2 = System.currentTimeMillis() - time2;
+        System.out.println("Время "+time2);*/
+
+
+    }
+
+    public static ArrayList<Student> getStudents (String file,int iname, int iemail, int iid,int iphone ) {
 
         emailindex = iemail;
         vkindex = iid;
         phoneindex = iphone;
 
-        FileInputStream fis = new FileInputStream(file);
+        Workbook wb = null;
 
-        Workbook wb = new XSSFWorkbook(fis);
+        try (InputStream in = new BufferedInputStream(new FileInputStream(file))) {
+
+            wb = new XSSFWorkbook(in);
+        }
+        catch (IOException e){
+
+            e.printStackTrace();
+        }
+
         ArrayList<Student> list = new ArrayList();
 
-
         String name;
-        String surname;
         String email;
         String phone;
         String id;
 
         for (Row row: wb.getSheetAt(0)) {
-                name = (getCellText(row.getCell(iname)));
-                surname = getCellText(row.getCell(iSurname));
-                email = (getCellText(row.getCell(iemail)));
-                id = (getCellText(row.getCell(iid)));
-                phone = phoneconvert(getCellText(row.getCell(iphone)));
 
-                list.add(new Student(name, surname, email, phone, id));
+            name = (getCellText(row.getCell(iname)));
+            email = (getCellText(row.getCell(iemail)));
+            id = (getCellText(row.getCell(iid)));
+            phone = phoneconvert(getCellText(row.getCell(iphone)));
+
+            list.add(new Student(name, email, phone, id));
         }
+
         list.remove(0);
         list.trimToSize();
 
-
-
-        fis.close();
         return list;
     }
 
+    public static ArrayList<String> getTitles(String file) {
 
-    public static ArrayList<String> getTitles(String file) throws IOException {
+        Workbook wb = null;
 
-        FileInputStream fis = new FileInputStream(file);
-        Workbook wb = new XSSFWorkbook(fis);
+        try (InputStream in = new BufferedInputStream(new FileInputStream(file))) {
+
+            wb = new XSSFWorkbook(in);
+        }
+        catch (IOException e){
+
+            e.printStackTrace();
+        }
+
         ArrayList<String> list = new ArrayList();
 
         Row row = wb.getSheetAt(0).getRow(0);
@@ -68,37 +136,44 @@ public class ParserImpl implements Parser {
         }
         list.remove(0);
 
-        fis.close();
         return list;
     }
 
-    public static void log(String file, ArrayList<Student> list,boolean isSendVk, boolean isSendMail, boolean isSendPhone) throws IOException {
+    public static void log(String file, ArrayList<Student> list,boolean isSendVk, boolean isSendMail, boolean isSendPhone) {
 
-        FileInputStream fis = new FileInputStream(file);
+        Workbook wb = null;
 
-        Workbook wb = new XSSFWorkbook(fis);
+        try (InputStream in = new BufferedInputStream(new FileInputStream(file))) {
 
+            wb = new XSSFWorkbook(in);
 
-        CellStyle redstyle = wb.createCellStyle();
+        }   catch (IOException e){
+
+            e.printStackTrace();
+        }
+
+        CellStyle redstyle   = wb.createCellStyle();
         CellStyle greenstyle = wb.createCellStyle();
+
         redstyle.setFillForegroundColor(IndexedColors.RED.getIndex());
         redstyle.setFillPattern(XSSFCellStyle.SOLID_FOREGROUND);
+
         greenstyle.setFillForegroundColor(IndexedColors.BRIGHT_GREEN.getIndex());
         greenstyle.setFillPattern(XSSFCellStyle.SOLID_FOREGROUND);
 
         int i = 1;
 
-        for (Student student : list){
+        for (Student student : list) {
 
             Row row = wb.getSheetAt(0).getRow(i);
 
-            if ((student.getIsSendVk()) &&(isSendVk)) {
+            if ((student.getIsSendVk()) && (isSendVk)) {
                 row.getCell(vkindex).setCellStyle(greenstyle);
-            } else if ((!(student.getIsSendVk())) && (isSendVk)){
+            } else if ((!(student.getIsSendVk())) && (isSendVk)) {
                 row.getCell(vkindex).setCellStyle(redstyle);
             }
 
-            if ((student.getIsSendMail()) && (isSendMail)){
+            if ((student.getIsSendMail()) && (isSendMail)) {
                 row.getCell(emailindex).setCellStyle(greenstyle);
             } else if ((!(student.getIsSendMail())) && (isSendMail)) {
                 row.getCell(emailindex).setCellStyle(redstyle);
@@ -112,13 +187,15 @@ public class ParserImpl implements Parser {
             i++;
         }
 
-        FileOutputStream fos = new FileOutputStream(file);
-        wb.write(fos);
-        fos.flush();
-        fos.close();
-        fis.close();
-    }
+        try (OutputStream out = new BufferedOutputStream(new FileOutputStream("Результаты рассылки.xlsx"))) {
 
+            wb.write(out);
+            out.flush();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
 
     public static String getCellText(Cell cell){
@@ -147,24 +224,25 @@ public class ParserImpl implements Parser {
     }
 
     static String phoneconvert(String phone){
-        String s = "";
-        int i;
-        for(i = 0; i<phone.length();i++) {
+
+        StringBuilder s = new StringBuilder("");
+
+        for(int i = 0; i<phone.length();i++) {
             if (Character.isDigit(phone.charAt(i))) {
-                s = s + phone.charAt(i);
+
+                s.append(phone.charAt(i));
             }
         }
-            if ( (s.length()>0)&& (s.charAt(0) == '8') && (s.length() == 11) ) {
+        if ( (s.length()>0) && (s.charAt(0) == '8') && (s.length() == 11) ){
 
-                String str = "7";
-                for (i = 1; i<s.length();i++){
+            s.setCharAt(0,'7');
 
-                    str = str + s.charAt(i);
-                }
-                s = str;
-            } else if ((s.length()>0)&&(s.length() == 10) ) {
-                s = 7 + s;
-            }
-        return s;
+        } else if ((s.length()>0) && (s.length() == 10) ) {
+
+            s.insert(0,'7');
+        }
+
+        return s.toString();
     }
+
 }
