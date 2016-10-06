8c273cc87169fbbaf8abe48a8bc3a5ced611e098
vadim0872 <vadim0872@mail.ru>
2016-10-07 01:13:04 +0400

    Refresh fields

--- src/main/java/org/students/Student.java
+++ src/main/java/org/students/Student.java
@@ -1,43 +1,35 @@
 package org.students;
 
 /**
- * Created by aleksejpluhin on 06.04.16.
+ * Created by Вадим on 10.05.2016.
+ *
  */
 public class Student {
+
     private String name;
-    private String surname;
     private String mail;
     private String phone;
     private String vk_id;
+    private String messageText;
+
     private boolean isSendPhone = false;
     private boolean isSendVk = false;
     private boolean isSendMail = false;
-    private String messageText;
 
 
-    public Student(String name, String surname, String mail, String phone, String vk_id) {
+
+
+    public Student(String name, String mail, String phone, String vk_id) {
         this.name = name;
-        this.surname = surname;
         this.mail = mail;
         this.phone = phone;
         this.vk_id = vk_id;
 
     }
 
-    public void setMessageText(String messageTest) {
-        this.messageText = messageTest;
-    }
 
     public String getName() {
         return name;
-    }
-
-    public String getMessageText() {
-        return messageText;
-    }
-
-    public String getSurname() {
-        return surname;
     }
 
     public String getMail() {
@@ -48,40 +40,44 @@ public class Student {
         return phone;
     }
 
-
-
     public String getVk_id() {
         return vk_id;
     }
 
+
+    public void setMessageText(String messageTest) {
+        this.messageText = messageTest;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+
     public void setIsSendPhone() {
         this.isSendPhone = true;
-    }
-
-    public void setIsSendVk() {
-        this.isSendVk = true;
-    }
-
-    public void setIsSendMail() {
-        this.isSendMail = true;
     }
 
     public boolean getIsSendPhone(){
         return isSendPhone;
     }
 
+
+    public void setIsSendVk() {
+        this.isSendVk = true;
+    }
+
     public boolean getIsSendVk(){
         return isSendVk;
+    }
+
+
+    public void setIsSendMail() {
+        this.isSendMail = true;
     }
 
     public boolean getIsSendMail(){
         return isSendMail;
     }
 
-    @Override
-    public String toString() {
-        return "Студент: " + name + " " + surname +
-                " Почта " + isSendMail + " Вконтакте " + isSendVk +
-                " Телефон " + isSendPhone;
-    }
 }
